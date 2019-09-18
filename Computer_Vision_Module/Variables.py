110badc6966f72a099480d4fa0bcdac835f14b11
Mohammed Samir <mohammed.samir.mahmoud@gmail.com>
2019-09-18 14:31:32 +0200

    Add Load Dataset Function Adding Required Variables

--- Computer_Vision_Module/Variables.py
+++ Computer_Vision_Module/Variables.py
@@ -1,4 +1,11 @@
+import os
 import mtcnn
 from mtcnn.mtcnn import MTCNN
 
 detector = MTCNN() # Creating instance from the class MTCNN
+
+# EMBEDDINGS PATH
+EMBEDDINGS_PATH = os.getcwd() 
+
+# EMPLOYEES PATH
+EMPLOYEES_NAMES = os.getcwd()
